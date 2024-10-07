75999f2fd3dc5e8b1077ca956cb966f861e1105d
jibolashittubolu <54463012+jibolashittubolu@users.noreply.github.com>
2024-10-07 01:15:57 +0100

    Update index.ts

--- clientnext/src/components/progress/index.ts
+++ clientnext/src/components/progress/index.ts
@@ -1,7 +1,7 @@
 // export * from "./ErrorHandling";
 // export { default as successResponse } from "./successHandling";
 // export {axiosFunctions} from './axiosFunctions'
-
+ 
 // export * from "./axiosFunctions";
 export { default as LinearIndeterminateProgressBar } from "./LinearIndeterminateProgressBar.progressbar"; //if you exported as const from axiosFunctions.ts
 // export { default as axiosFunctions } from "./axiosFunctions"; // if you exported as default from axiosFunctions.ts
