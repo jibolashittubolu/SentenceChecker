fe82f3894d47a51e7db67fccdeb67c10bb899176
jibolashittubolu <54463012+jibolashittubolu@users.noreply.github.com>
2025-02-19 00:02:38 +0100

    Update mainV1.py

--- serverfastapi/app/api/v1/mainV1.py
+++ serverfastapi/app/api/v1/mainV1.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from app.api.v1.routers import user, item, sentenceChecker
 
 v1 = FastAPI()
-
 v1.include_router(user.router, prefix="/users", tags=["users"])
 v1.include_router(item.router, prefix="/items", tags=["items"])
 v1.include_router(sentenceChecker.router, prefix="/checkSentence", tags=["checkSentence"])
